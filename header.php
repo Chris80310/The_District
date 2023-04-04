88009b81de8a4a612bbfecabc5d1e78e27903b9b
chambonnet christopher <chris80780@yahoo.fr>
2023-04-04 17:00:53 +0200

    com1

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header class="bg_header">
+
+        <div class="container-fluid p-0">
+            <div class="col d-flex div_header">
+                <!-- banière / logo :  --> 
+                <img src="/assets/images_the_district/the_district_brand/logo_transparent.png" height="300px">
+            </div>
+            
+            <nav class="navbar navbar-expand-lg p-0">
+                <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+
+                <div class="col navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" style="color:white" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color:white" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color:white" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" style="color:white" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+</body>
+</html>
